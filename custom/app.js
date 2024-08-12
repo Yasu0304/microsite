e3bbec992d9549f5be79da5a6bae2a45c93d408e
Yasu <blackcat7134@gmail.com>
2024-08-12 09:55:13 +0800

    hide

--- custom/app.js
+++ custom/app.js
@@ -1,16 +1,12 @@
-let medobj = [
+let medObj = [
     {
         aiItem: "醫療影像處理",
         aiName: '3D成像',
-
-
     },
     {
         aiItem: "醫療影像處理",
         aiName:
             '圖像增強和分割',
-
-
     }, {
         aiItem: "健康管理",
         aiName:
@@ -19,7 +15,6 @@ let medobj = [
     {
         aiItem: "健康管理",
         aiName: '預防保健',
-
     },
     {
         aiItem: "疾病診斷",
@@ -52,23 +47,26 @@ let medobj = [
     {
         aiItem: "臨床決策支持",
         aiName: '癒後評估',
-
     }
 
 ]
 
 let cardContent = document.querySelector(".col.cardContent")
-console.log(cardContent);
+// console.log(cardContent);
 let cardGroup = document.querySelector(".row.row-cols-1.row-cols-md-3.g-4.cardGroup")
-console.log(cardGroup);
-cardGroup.innerHTML = `<div class="col cardContent">
-                <div class="card h-100">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${medobj[0].aiItem}</h5>
-                        <p>${medobj[0].aiName}</p>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            </div>`;
+medObj.forEach((item, index) => {
+    console.log(item.aiItem, item.aiName)
+})
+
+// console.log(cardGroup);
+// cardGroup.innerHTML = `<div class="col cardContent">
+//                 <div class="card h-100">
+//                     <img src="..." class="card-img-top" alt="...">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${medobj[0].aiItem}</h5>
+//                         <p>${medobj[0].aiName}</p>
+//                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+//                             additional content. This content is a little bit longer.</p>
+//                     </div>
+//                 </div>
+//             </div>`;
