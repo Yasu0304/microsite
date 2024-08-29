493e268a1f6cf233ecde76863ebbda594dac95d3
anyu8242 <anyu8242@gmail.com>
2024-08-29 17:35:14 +0800

    aiImg

--- custom/js/index.js
+++
@@ -1,9 +0,0 @@
-let readBtn = document.querySelector(`.readBtn`)
-let cardDetail = document.querySelector(`.cardDetail`);
-
-readBtn.addEventListener("click", e => {
-    cardDetail.classList.toggle(`open`);
-    let text = document.createElement('p');
-    cardDetail.appendChild(text)
-    text.textContent = '替換文字節點';
-})
